e854b7690e909fe860060461617be24ea738d94b
Mathias Tranberg <Mathias Ttranbergmathias@gmail.comranberg>
2023-12-18 10:59:03 +0000

    file.h

--- main.c
+++ main.c
@@ -1,14 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <ctype.h>
-#define ADD_VEHICLE 1
-#define REMOVE_VEHICLE 2
-#define SORT 3
-#define SHOW_REGISTRY 4
-#define SHOW_VEHICLE 5
-#define QUIT 0
+
 
 int main() {
     
